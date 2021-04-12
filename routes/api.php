7bb7fdd32fdf1238e3f9a3ad93d0c43706b9b6bf
Ade Fian Galih I <fianade8@gmail.com>
2021-04-12 12:26:10 +0700

    Upload Foto & Kompres - DriverApi  --Thumbnail 200

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('driver-register', 'AuthController@register_driver');
+<<<<<<< HEAD
+=======
+Route::post('driver-update_profile', 'DriverController@update');
+Route::get('driver-get_posting/{id}', 'DriverController@get_posting_driver');
+Route::post('driver-posting/{id}', 'DriverController@posting_driver');
+
+>>>>>>> parent of 8345fde... Upload foto DriverApi --Thumbnail 200
 
 Route::post('driver-update_profile', 'DriverController@update');
 Route::get('driver-get_posting/{id}', 'DriverController@get_posting_driver');
