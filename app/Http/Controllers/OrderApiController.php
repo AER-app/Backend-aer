7bc12f7e360ee9c19f1b4b4e843b9fb44e59922a
Andy Ferbiantoro <andyfebri742@gmail.com>
2021-04-25 11:12:20 +0700

    order get jastip

--- app/Http/Controllers/OrderApiController.php
+++ app/Http/Controllers/OrderApiController.php
@@ -13,6 +13,7 @@ use App\Jastip;
 use App\OrderDetail;
 use App\OrderDetailOffline;
 use App\OrderOffline;
+use DB;
 
 use Illuminate\Http\Request;
 
@@ -40,6 +41,7 @@ class OrderApiController extends Controller
         	'longitude' => $request->longitude,
         	'latitude' => $request->latitude,
         	'status_order' => 'waiting',
+            'jarak' => $request->jarak,
         ]);
 
 		$lastid = Order::create($data)->id;
@@ -50,7 +52,6 @@ class OrderApiController extends Controller
 		        'id_jastip' => $id_jastip,
 		        'no_telp' => $no_telp,
 		        'note' => $note,
-		        'jarak' => $jarak,
 		        'harga' => $harga
                 
         ]);
@@ -74,6 +75,112 @@ class OrderApiController extends Controller
 	}
 
 
+    public function order_driver_get_order()
+    {
+        
+        $driver = Driver::all();
+        $hitung = new Haversine();
+        
+        $lapak = DB::table('order')
+        ->join('lapak', 'order.id_lapak', '=', 'lapak.id')
+        ->where('id_driver', null)
+        ->select('lapak.id_kecamatan1')->first();
+        // dd($lapak);
+        $length = count($driver);
+        $tes = null;
+        $show_order = null;
+
+
+        for ($i=0; $i < $length ; $i++) {
+            if ($driver[$i]->id_kecamatan1 == $lapak->id_kecamatan1 || $driver[$i]->id_kecamatan2 == $lapak->id_kecamatan1) {
+        
+        $show_order = DB::table('order')
+        ->join('lapak', 'order.id_lapak', '=', 'lapak.id')
+        ->join('customer', 'order.id_customer', '=', 'customer.id')
+        ->select('order.*', 'lapak.latitude_lap', 'lapak.longitude_lap', 'lapak.id_kecamatan1', 'lapak.id_kecamatan2', 'customer.latitude_cus', 'customer.longitude_cus')
+        ->where('id_driver', null)
+        ->orderBy('id','DESC')
+        ->first();
+
+                $tes[] = $driver[$i];
+            }
+        }
+
+        $lat_lapak = $show_order->latitude_lap;
+        $long_lapak = $show_order->longitude_lap;
+
+        $hasil = array();
+        foreach ($tes as $value) {
+
+
+            $jarak = round($hitung->distance($value->latitude_driver, $value->longitude_driver, $lat_lapak, $long_lapak, "K"), 1);
+            $hasil[] =['orderan' =>$show_order,'KM' => $jarak,'id'=>$value->id_user] ;
+        }
+       
+        $c = collect($hasil);
+        $sort = $c->SortBy('KM');
+        return $sort->values()->all();
+        
+        
+        //    return response()->json(['driver' => $tes, 'order' => $show_order, 'jarak' => $hasil], 200, );
+    }
+
+    //driver menerima orderan
+    public function order_driver_terima_order(Request $request, $id){
+
+        $terima_order = Order::findOrFail($id);
+
+        $data = [
+            'id_driver' => $request->id_driver,
+            'status' => 'proses',
+           
+        ];
+
+        if ($terima_order->update($data)) {
+            $out = [
+                "message" => "success",
+                "code" => 201
+            ];
+        } else {
+            $out = [
+                "message" => "vailed",
+                "code" => 404
+            ];
+        }
+        
+
+        return response()->json($out, $out['code']);
+
+    }
+
+    //get menu jastip
+    public function order_get_menu_jastip(){
+
+        $jastip = DB::table('order_detail')
+            ->join('menu', 'order_detail.id_menu', '=', 'menu.id')
+            ->join('order', 'order_detail.id_order', '=', 'order.id')
+            ->select('order_detail.*', 'menu.nama_menu','menu.diskon','order.jarak') 
+            ->get();
+
+
+            $data = [];
+            foreach ($jastip as $jast) {
+                # code...
+                //$customer = Customer::where('id_user',$id_user)->first();     
+                $diskon = $jast->harga * ($jast->diskon / 100);
+                $data[] = [
+                    'menu' => $jast,
+                    'harga_diskon' => $jast->harga-$diskon,
+                ];
+            }
+
+             return response()->json([
+
+            'Hasil Menu jastip' => $data
+        ]);     
+    }
+
+
 	//proses tambah jastip dari orderan yang muncul
 	public function order_tambah_jastip(Request $request){
 
@@ -89,16 +196,17 @@ class OrderApiController extends Controller
         	'id_order' => $request->id_order,
         	'id_driver' => $request->id_driver,
         	'id_customer' => $request->id_customer,
+            'id_menu' => $request->id_menu,
         	'kode_jastip' => $request->kode_jastip,
         	'status_jastip' => $request->status_jastip,
         ]);
 
-		$lastid = Jastip::create($data)->id;
+		$lastid = Jastip::create($data)->id; 
 
 		$order_detail = OrderDetail::create([
                 'id_order' => $id_order,
-		        'id_menu' => $id_menu,
-		        'id_jastip' => $lastid,
+		        '' => $id_menu,
+		        'id_jid_menuastip' => $lastid,
 		        'no_telp' => $no_telp,
 		        'note' => $note,
 		        'jarak' => $jarak,
@@ -107,7 +215,6 @@ class OrderApiController extends Controller
         ]);
 
 
-     	
         if ($lastid && $order_detail) {
             $out = [
                 "message" => "tambah-jastip_success",
@@ -127,7 +234,7 @@ class OrderApiController extends Controller
 
 
 	//proses tambah jastip dari orderan yang muncul
-	public function order_tambah_order_customer_offline(Request $request){
+	public function order_tambah_order_customer_offline (Request $request){
 
 		
         $id_menu = $request->id_menu;
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::post('driver-register', 'AuthApiController@register_driver');
 Route::post('driver-update_profile', 'DriverApiController@update');
 Route::get('driver-get_posting/{id}', 'DriverApiController@get_posting_driver');
 Route::post('driver-posting/{id}', 'DriverApiController@posting_driver');
+Route::post('driver-terima_order/{id}', 'DriverApiController@driver_terima_order');
 
 
 //Lapak
@@ -31,17 +32,20 @@ Route::post('/lapak-update/{id_user}','LapakApiController@lapak_update');
 Route::post('/lapak-tambah_menu','LapakApiController@lapak_tambah_menu');
 Route::get('/lapak-get_menu/{id}','LapakApiController@lapak_get_menu');
 Route::get('/lapak-get_posting_lapak/{id}','LapakApiController@lapak_get_posting_lapak');
-Route::get('/lapak-get_profil/{id}','LapakApiController@lapak_get_profil');
+Route::get('/lapak-get_profile/{id}','LapakApiController@lapak_get_profile');
 Route::post('/lapak-tambah_posting','LapakApiController@lapak_tambah_posting');
+Route::get('/lapak-get_kategori','LapakApiController@lapak_get_kategori');
+
 
 
 
 //Customer
 Route::post('/customer-register','AuthApiController@customer_register');
-Route::get('/customer-get_profil/{id}','CustomerApiController@customer_get_profil');
+Route::get('/customer-get_profile/{id}','CustomerApiController@customer_get_profile');
 Route::post('/customer-update_profile/{id}','CustomerApiController@customer_update_profile');
-Route::get('/customer-get_menu_all/{id_user}','CustomerApiController@customer_get_menu_all');
+Route::get('/customer-get_menu_all','CustomerApiController@customer_get_menu_all');
 Route::get('/customer-get_menu_terbaru','CustomerApiController@customer_get_menu_terbaru');
+Route::get('/customer-get_posting_driver_all','CustomerApiController@customer_get_posting_driver_all');
 Route::get('/customer-get_menu_terlaris','CustomerApiController@customer_get_menu_terlaris');
 Route::get('/customer-cari_menu','CustomerApiController@customer_cari_menu');
 Route::get('/customer-get_detail_menu/{id}','CustomerApiController@customer_get_detail_menu');
@@ -54,4 +58,8 @@ Route::get('/customer-hitung','CustomerApiController@hitung');
 Route::post('/order-tambah_order','OrderApiController@order_tambah_order');
 Route::post('/order-tambah_jastip','OrderApiController@order_tambah_jastip');
 Route::post('/order-tambah_order_customer_offline','OrderApiController@order_tambah_order_customer_offline');
+Route::get('/order-driver-get_order', 'OrderApiController@order_driver_get_order');
+Route::get('/order-get_menu_jastip', 'OrderApiController@order_get_menu_jastip');
+Route::post('/order-driver-terima_order/{id}', 'OrderApiController@order_driver_terima_order');
+
 
